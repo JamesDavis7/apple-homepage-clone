128a1c4669a4f40764afb982d597f63fed6aadd2
James Davis <jdav2208@gmail.com>
2022-04-24 13:40:59 +0100

    stuff removed

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-// mobile navbar dropdown
-
 const selectElement = (element) => document.querySelector(element);
 
 selectElement(".mobile-menu").addEventListener("click", () => {
   selectElement("header").classList.toggle("active");
 });
 
-// end of mobile navbar dropdown
-
-// mobile footer content dropdown
-
 const dropper = document.getElementsByClassName("dropper");
 let i;
 
 for (i = 0; i < dropper.length; i++) {
   dropper[i].addEventListener("click", function () {
-    // loops over each dropper and adds the click event
     this.classList.toggle("active");
-    let dropdownContent = this.nextElementSibling; // nextElementSibling is the elements with the 'dropdown-content' class...
+    let dropdownContent = this.nextElementSibling;
     if (
-      dropdownContent.style.maxHeight && // prevents double click issue
+      dropdownContent.style.maxHeight &&
       dropdownContent.style.maxHeight != "0px"
     ) {
       dropdownContent.style.maxHeight = 0;
@@ -28,5 +21,3 @@ for (i = 0; i < dropper.length; i++) {
     }
   });
 }
-
-// end of mobile footer content dropdown
